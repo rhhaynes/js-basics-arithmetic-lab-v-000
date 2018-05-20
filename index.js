3cec3302f09a5991219c9be4f76508d75882cb87
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-20 16:57:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const newID = oldID + 1000000000;
 
 const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumer = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1e9;
